66cc8e14e285fa53e02dd7cd7c167705495901e8
bnies1017 <ben.nies1017@gmail.com>
2023-03-04 13:48:13 -0700

    download some TSLA stock data

---
+++ init_data.py
@@ -0,0 +1,10 @@
+import services.stock_data
+import pandas as pd
+
+symbol = 'TSLA'
+timeframe = '1min'
+start = '2020-01-01T00:00:00Z'
+end = '2022-03-01T00:00:00Z'
+
+TSLA_data = pd.DataFrame(services.stock_data.get_historical_data(symbol, timeframe, start, end))
+TSLA_data.to_csv("data/train/{symbol}-{timeframe}-{start}-{end}.csv".format(symbol=symbol, timeframe=timeframe, start=start, end=end))
