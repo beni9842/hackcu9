9af5a2ecad8adec170ed1d86860c56a767225d07
killianblitz <kbl1393@colorado.edu>
2023-03-04 16:07:57 -0700

    able to write csv

--- init_data.py
+++ init_data.py
@@ -15,8 +15,15 @@ end = '2022-03-01T00:00:00Z'
 TSLA_data = pd.DataFrame(services.stock_data.get_historical_data(symbol, timeframe, start, end))
 TSLA_data.to_csv("data/train/{symbol}-{timeframe}-{start}-{end}.csv".format(symbol=symbol, timeframe=timeframe, start=start, end=end))
 
-symbol= ['TSLA', 'AMZN','APPL','GOOG','ATVI','F','BRK-B']
+symbols= ['TSLA', 'AMZN','APPL','GOOG','ATVI','F','BRK-B']
 start_times = ['2020-12-01T00:00:00Z','2015-12-15T00:00:00Z', '2021-03-01T00:00:00Z', '2023-02-14T00:00:00Z', '2017-12-25T00:00:00Z']
 end = '2022-03-01T00:00:00Z'
 
+dfs = []
+for symbols in stickers:
+    df = pd.DataFrame(services.stock_data.get_historical_data(symbol, timeframe, start, end))
+    dfs.append(df)
+
+result = pd.concat(dfs)
+result.to_csv('historical_stock_data.csv', index=False)
 
