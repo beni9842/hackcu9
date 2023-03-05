56171bb706393096dc9d3dc9241d98d1c0b7ac17
bnies1017 <ben.nies1017@gmail.com>
2023-03-05 03:12:47 -0700

    graphs in repo

--- model.py
+++ model.py
@@ -4,65 +4,82 @@ import tensorflow as tf
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, LSTM, Dropout
+import matplotlib.pyplot as plt
+import os
+
+def get_predictions(df):
+    # Preprocess data
+    scaler = MinMaxScaler()
+    scaled_data = scaler.fit_transform(df.values.reshape(-1, 1))
+
+    # Split data into training and testing sets
+    training_data_len = int(np.ceil(len(scaled_data) * 0.8))
+    train_data = scaled_data[0:training_data_len, :]
+    x_train = []
+    y_train = []
+
+    for i in range(60, len(train_data)):
+        x_train.append(train_data[i-60:i, 0])
+        y_train.append(train_data[i, 0])
+
+    x_train, y_train = np.array(x_train), np.array(y_train)
+    x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
+
+    # Build model
+    model = Sequential()
+    model.add(LSTM(units=50, return_sequences=True, input_shape=(x_train.shape[1], 1)))
+    model.add(Dropout(0.2))
+    model.add(LSTM(units=50, return_sequences=True))
+    model.add(Dropout(0.2))
+    model.add(LSTM(units=50))
+    model.add(Dropout(0.2))
+    model.add(Dense(units=1))
+
+    # Compile model
+    model.compile(optimizer='adam', loss='mean_squared_error')
+
+    # Train model
+    model.fit(x_train, y_train, epochs=100, batch_size=32)
+
+    # Test model
+    test_data = scaled_data[training_data_len - 60:, :]
+    x_test = []
+    y_test = df['close'][training_data_len:].values
+
+    for i in range(60, len(test_data)):
+        x_test.append(test_data[i-60:i, 0])
+
+    x_test = np.array(x_test)
+    x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
+
+    predictions = model.predict(x_test)
+    predictions = scaler.inverse_transform(predictions)
+
+    # Evaluate model
+    rmse = np.sqrt(np.mean((predictions - y_test)**2))
+    print(rmse)
+
+    return predictions[:,0], y_test
+
 
 # Load data
-df = pd.read_csv('data/train/AAPL-1D-2016-02-14T00:00:00Z.csv').loc[:,['close']]
+atvi = ('atvi', pd.read_csv('data/prices/atvi.csv').loc[:,['close']])
+bmbl = ('bmbl', pd.read_csv('data/prices/bmbl.csv').loc[:,['close']])
+tsla = ('tsla', pd.read_csv('data/prices/tsla.csv').loc[:,['close']])
 
-# Preprocess data
-scaler = MinMaxScaler()
-scaled_data = scaler.fit_transform(df.values.reshape(-1, 1))
-
-# Split data into training and testing sets
-training_data_len = int(np.ceil(len(scaled_data) * 0.8))
-train_data = scaled_data[0:training_data_len, :]
-x_train = []
-y_train = []
-
-for i in range(60, len(train_data)):
-    x_train.append(train_data[i-60:i, 0])
-    y_train.append(train_data[i, 0])
-
-x_train, y_train = np.array(x_train), np.array(y_train)
-x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], 1))
-
-# Build model
-model = Sequential()
-model.add(LSTM(units=50, activation='relu', return_sequences=True, input_shape=(x_train.shape[1], 1)))
-model.add(Dropout(0.2))
-model.add(LSTM(units=50, activation='relu', return_sequences=True))
-model.add(Dropout(0.2))
-model.add(LSTM(units=50))
-model.add(Dropout(0.2))
-model.add(Dense(units=1))
-
-# Compile model
-model.compile(optimizer='adam', loss='mean_squared_error')
-
-# Train model
-model.fit(x_train, y_train, epochs=100, batch_size=32)
-
-# Test model
-test_data = scaled_data[training_data_len - 60:, :]
-x_test = []
-y_test = df['close'][training_data_len:].values
-
-for i in range(60, len(test_data)):
-    x_test.append(test_data[i-60:i, 0])
-
-x_test = np.array(x_test)
-x_test = np.reshape(x_test, (x_test.shape[0], x_test.shape[1], 1))
-
-predictions = model.predict(x_test)
-predictions = scaler.inverse_transform(predictions)
-
-# Evaluate model
-rmse = np.sqrt(np.mean((predictions - y_test)**2))
-print(rmse)
+datasets = [atvi, bmbl, tsla]
+for dataset in datasets:
+    symbol, df = dataset
+    pred, real = get_predictions(df)
 
 
-# Plot the results
-import matplotlib.pyplot as plt
-plt.plot(y_test)
-plt.plot(predictions[:,0])
-plt.legend(['Actual', 'Predicted'])
-plt.show()
+    os.makedirs("data/pred", exist_ok=True)
+    os.makedirs("data/img", exist_ok=True)
+    pd.DataFrame(pred).to_csv("data/pred/{symbol}.csv".format(symbol=symbol))
+
+    # Plot the results
+    plt.plot(real)
+    plt.plot(pred)
+    plt.legend(['Actual', 'Predicted'])
+    plt.savefig("data/img/{symbol}.png".format(symbol=symbol))
+    plt.close()
--- trade_sim.py
+++ trade_sim.py
@@ -1,19 +1,14 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Define the asset and time period to retrieve data for
-symbol = 'AAPL'
-start = '2020-01-01'
-end = '2020-12-31'
 
 # Retrieve historic data for the asset
-prices = pd.read_csv('data/train/ATVI-1D-2016-02-14T00:00:00Z.csv')['close'].values[800:]
 
 # Define the initial portfolio balance and trade size
-portfolio_balance = 1000000
-n_shares = 0
 
-def dca(n_buys=25, n_shares=0, portfolio_balance=100000):
+def dca(prices, n_buys=25, n_shares=0, portfolio_balance=100000):
     trade_interval = len(prices) / n_buys
     for i in range(len(prices)):
         if i % trade_interval == 0:
@@ -38,7 +33,7 @@ def get_critical_numbers(arr):
 
     return critical_numbers
 
-def predict_buy(predictions, n_shares=0, portfolio_balance=100000, risk=10):
+def predict_buy(prices, predictions, n_shares=0, portfolio_balance=100000, risk=2):
     critical_days = get_critical_numbers(predictions)
     for i in range(len(prices)):
         if i in critical_days:
@@ -46,21 +41,34 @@ def predict_buy(predictions, n_shares=0, portfolio_balance=100000, risk=10):
             next_critical_day = int(np.where(critical_days==i)[0]) + 1
             next_critical_price = predictions[next_critical_day]
             if next_critical_price > today:
-                buy_amount = (next_critical_price - today)**risk
-                if (portfolio_balance > buy_amount):
+                buy_amount = min(portfolio_balance, (next_critical_price - today)**risk)
+                if (portfolio_balance >= buy_amount):
                     portfolio_balance -= buy_amount
                     n_shares += buy_amount / today
+            elif next_critical_price < today:
+                sell_amount = min(n_shares*today, (today - next_critical_price)*risk)
+                portfolio_balance += sell_amount
+                n_shares -= sell_amount / today
     final_price = prices[-1]
     portfolio_balance += n_shares * final_price
     return portfolio_balance
 
-print(dca())
-predictions = pd.read_csv("data/pred.csv")['0'].values
-print(predict_buy(predictions=predictions))
+tsla = pd.read_csv('data/prices/tsla.csv')['close'].values[800:]
+atvi = pd.read_csv('data/prices/atvi.csv')['close'].values[800:]
 
-# Plot the results
-import matplotlib.pyplot as plt
-plt.plot(prices)
-plt.plot(predictions)
-plt.legend(['Actual', 'Predicted'])
-plt.show()
+datasets = [('tsla', tsla), ('atvi', atvi)]
+for symbol, prices in datasets:
+    prices = pd.read_csv('data/prices/{symbol}.csv'.format(symbol=symbol))['close'].values[800:]
+
+    pred = pd.read_csv("data/pred/{symbol}.csv".format(symbol=symbol))['0'].values
+    real = prices
+
+    bal = 100000
+    n_shares = 0
+    print(dca(prices=prices, portfolio_balance=bal, n_shares=n_shares, n_buys=25))
+    print(predict_buy(prices=prices, predictions=pred, portfolio_balance=bal, n_shares=n_shares, risk=2))
+
+# plt.plot(real)
+# plt.plot(pred)
+# plt.legend(['Actual', 'Predicted'])
+# plt.show()
